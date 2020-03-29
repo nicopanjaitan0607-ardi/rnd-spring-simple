3816e5447bd6925cd7839b0e87bb0cf430d4d3a9
Edwin <edwinkun@gmail.com>
2020-03-29 20:27:19 +0700

    removing fabric8

---
+++ src/main/java/com/edw/test/demo/Something.java
@@ -0,0 +1,12 @@
+package com.edw.test.demo;
+
+/**
+ * <pre>
+ *     com.edw.test.demo.Something
+ * </pre>
+ *
+ * @author Muhammad Edwin < emuhamma at redhat dot com >
+ * 07 Nov 2019 23:58
+ */
+public class Something {
+}
---
+++ src/test/java/com/edw/test/demo/SomethingTest.java
@@ -0,0 +1,12 @@
+package com.edw.test.demo;
+
+/**
+ * <pre>
+ *     com.edw.test.demo.SomethingTest
+ * </pre>
+ *
+ * @author Muhammad Edwin < emuhamma at redhat dot com >
+ * 08 Nov 2019 00:00
+ */
+public class SomethingTest {
+}
